5e417acb5dde65a3056dfdfe8d0ec57b53b93ae3
MrSybaro <syabro1998@gmail.com>
2023-08-06 19:00:48 +0300

    hotfix: теперь сначала ищется кешированные библиотеки

--- lib/https_package.lua
+++ lib/https_package.lua
@@ -5,8 +5,8 @@ local fmt = "/(%w+).lua$"
 
 return function(env)
     local function require(uri)
+        if loaded[uri] then return loaded[uri] end
         local name = uri:match(fmt)
-        if loaded[name] then return loaded[name] end
         local data = assert(https.request(uri))
         local res = assert(load(data, "uri", "t", env))()
         loaded[name] = res
